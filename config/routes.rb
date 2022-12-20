d738510f575154f306025894b4ef0fd402017f9c
jonasongg <jonasong2@gmail.com>
2022-12-20 20:46:19 +0800

    Added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   #Root path
   root 'homepage#index'
 
   namespace :api do
     namespace :v1 do
       resources :posts, except: [:new, :edit]
+      resources :users, only: [:show]
     end
   end
 
