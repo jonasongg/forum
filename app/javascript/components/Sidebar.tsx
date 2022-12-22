0c26e878b4b16cadc3badad53a71479edd77b811
jonasongg <jonasong2@gmail.com>
2022-12-22 22:07:02 +0800

    Added placeholder button for sidebar

--- app/javascript/components/Sidebar.tsx
+++ app/javascript/components/Sidebar.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Side = styled.aside`
+    display: flex;
+    gap: 20px;
+    justify-content: center;
+
+    font-size: 15px;
+    font-weight: 500;
     width: 25%;
     height: 100%;
     background-color: white;
@@ -9,8 +15,28 @@ const Side = styled.aside`
     box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.02);
 `;
 
+const CreateButton = styled.div`
+    width: 75%;
+    margin-top: 12%;
+    background: #404040
+        url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='white' class='bi bi-plus' viewBox='0 0 16 16'%3E%3Cpath d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'%3E%3C/path%3E%3C/svg%3E")
+        no-repeat 15px center;
+    border-radius: 10px;
+    padding: 8px 20px 8px 45px;
+    color: white;
+
+    box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.08);
+    :hover {
+        background-color: #282828;
+    }
+`;
+
 const Sidebar = () => {
-  return <Side>sidebar</Side>;
+  return (
+    <Side>
+      <CreateButton>Create a new post</CreateButton>
+    </Side>
+  );
 };
 
 export default Sidebar;
