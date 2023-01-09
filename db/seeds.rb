cf54251892d969f0f23f9e9f7b41ebe715c199d8
jonasongg <jonasong2@gmail.com>
2023-01-09 22:38:46 +0800

    Allowing reseeding of database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
 
 users = User.create([
   {
@@ -45,5 +48,14 @@ reply = Comment.create([
     user: users.first,
     post: posts.first,
     parent: comments.first
+  }
+])
+
+Comment.create([
+  {
+    body: "Donec ac scelerisque nulla.",
+    user: users.last,
+    post: posts.first,
+    parent: reply.first
   }
 ])
