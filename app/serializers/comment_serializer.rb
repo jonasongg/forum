f6d2f20296953c2b8965963f57f3c0b69efd0b06
jonasongg <jonasong2@gmail.com>
2023-01-18 20:55:08 +0800

    JSON to format date

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,11 +1,17 @@
 class CommentSerializer
   include JSONAPI::Serializer
-  attributes :body, :created_at
+  attributes :body
 
   attribute :user_username do |object|
     object.user.username
   end
 
+  #If the year of the date the post is created is this year, then omit the year
+  attribute :created_at do |object|
+    object.created_at.strftime(object.created_at.year == Time.now.year ? "%d %b" : "%d %b %Y")
+   end
+
+  # Serialise each reply also
   attribute :replies do |object|
     object.replies.map do |reply|
       CommentSerializer.new(reply)
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,13 +1,13 @@
 class PostSerializer
   include JSONAPI::Serializer
-  attributes :title, :body, :created_at
+  attributes :title, :body
 
   attribute :user_username do |object|
     object.user.username
   end
 
   #If the year of the date the post is created is this year, then omit the year
-  #attribute :created_at do |object|
-  #  object.created_at.strftime(object.created_at.year == Time.now.year ? "%d %b" : "%d %b %Y")
-  #end
+  attribute :created_at do |object|
+   object.created_at.strftime(object.created_at.year == Time.now.year ? "%d %b" : "%d %b %Y")
+  end
 end
